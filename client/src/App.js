b349bb4b904e661738c5ecfe95cc7fa42a0e073d
CanseFr <jucansell@gmail.com>
2024-10-26 12:45:13 +0200

    :sparkles: Add user service

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 function App() {
   const [invoice, setInvoice] = useState(null);
   const [devis, setDevis] = useState(null);
+  const [users, setUsers] = useState(null);
 
   useEffect(() => {
     const fetchInvoice = async () => {
@@ -29,33 +30,54 @@ function App() {
         });
         setDevis(response.data);
       } catch (error) {
-        console.error('Erreur lors de la récupération de la facture:', error);
+        console.error('Erreur lors de la récupération de devis:', error);
       }
     };
 
     fetchDevis();
   }
 
+  const handleGetUsers = () => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get('http://localhost/user', {
+          headers: { Authorization: "fake-token" } // Envoi du token d'authentification
+        });
+        setUsers(response.data);
+      } catch (error) {
+        console.error('Erreur lors de la récupération de user:', error);
+      }
+    };
+
+    fetchUsers();
+  }
+
   return (
       <div>
 
-      <div>
-        <h1>Facture</h1>
-        {invoice ? (
-            <div>
-              <p>Client: {invoice.client}</p>
-              <p>Montant: {invoice.montant} €</p>
-            </div>
-        ) : (
-            <p>Chargement...</p>
-        )}
-      </div>
+        <div>
+          <h1>Facture</h1>
+          {invoice ? (
+              <div>
+                <p>Client: {invoice.client}</p>
+                <p>Montant: {invoice.montant} €</p>
+              </div>
+          ) : (
+              <p>Chargement...</p>
+          )}
+        </div>
 
-      <div>
-        <p>les devis</p>
-        <button onClick={handleGetDevis}>Get devis</button>
-        {devis && (devis.map((item)=>item))}
-      </div>
+        <div>
+          <p>les devis</p>
+          <button onClick={handleGetDevis}>Get devis</button>
+          {devis && (devis.map((item) => item))}
+        </div>
+
+        <div>
+          <p>les client</p>
+          <button onClick={handleGetUsers}>Get users</button>
+          {users && (users.map((item) => item))}
+        </div>
       </div>
   );
 }
